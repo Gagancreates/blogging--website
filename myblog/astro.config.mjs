2e468183db2421e7d8e652373fce34fe0e7995ee
Gagan <gaganp000999@gmail.com>
2026-01-01 17:02:25 +0530

    new Blog on RNNs

--- myblog/astro.config.mjs
+++ myblog/astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
   adapter: netlify(),
   site: 'https://gaganp.com',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   build: {
     inlineStylesheets: 'auto'
   },
